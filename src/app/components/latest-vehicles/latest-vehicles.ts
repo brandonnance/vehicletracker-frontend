aad6486912ec0e798c7cbed288abbb1781d8b730
Brandon Nance <Brandon@GAME.local>
2025-11-14 11:19:42 -0800

    Fix leaflet icons using CDN

--- src/app/components/latest-vehicles/latest-vehicles.ts
+++ src/app/components/latest-vehicles/latest-vehicles.ts
@@ -11,6 +11,20 @@ import { LatestVehicle } from '../../models/latest-vehicle.model';
 import * as L from 'leaflet';
 import { isValidDate } from 'rxjs/internal/util/isDate';
 
+// Use Leaflet's CDN-hosted marker images
+const defaultIcon = L.icon({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl:       'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl:     'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize:      [25, 41],
+  iconAnchor:    [12, 41],
+  popupAnchor:   [1, -34],
+  shadowSize:    [41, 41],
+});
+
+// Make this the default icon for all markers
+(L.Marker as any).prototype.options.icon = defaultIcon;
+
 interface JobSummary {
   job_id: string | null;
   job_code: string;
